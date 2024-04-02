77a20fa93d025390e8d46721e0e4513253feaf31
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-04-02 15:10:45 -0600

    Creación de componentes

---
+++ controllers/admin/admin.js
@@ -0,0 +1,290 @@
+let SAVE_MODAL;
+let SAVE_FORM,
+    ID_ADMINISTRADOR,
+    NOMBRE_ADMINISTRADOR,
+    APELLIDO_ADMINISTRADOR,
+    CLAVE_ADMINISTRADOR,
+    CORREO_ADMINISTRADOR,
+    TELEFONO_ADMINISTRADOR,
+    DUI_ADMINISTRADOR,
+    NACIMIENTO_ADMINISTRADOR,
+    IMAGEN_ADMINISTRADOR,
+    REPETIR_CLAVE;
+let SEARCH_FORM;
+
+// Constantes para completar las rutas de la API.
+const ADMINISTRADOR_API = '';
+
+async function loadComponent(path) {
+    const response = await fetch(path);
+    const text = await response.text();
+    return text;
+}
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openCreate = () => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Crear administrador';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+}
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    try {
+        // Se define un objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idAdministrador', id);
+        // Petición para obtener los datos del registro solicitado.
+        const DATA = await fetchData(ADMINISTRADOR_API, 'readOne', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra la caja de diálogo con su título.
+            SAVE_MODAL.show();
+            MODAL_TITLE.textContent = 'Actualizar administrador';
+            // Se prepara el formulario.
+            SAVE_FORM.reset();
+            // Se inicializan los campos con los datos.
+            const ROW = DATA.dataset;
+            ID_ADMINISTRADOR.value = ROW.ID;
+            NOMBRE_ADMINISTRADOR.value = ROW.NOMBRE;
+            APELLIDO_ADMINISTRADOR.value = ROW.APELLIDO;
+            CORREO_ADMINISTRADOR.value = ROW.CORREO;
+            TELEFONO_ADMINISTRADOR.value = ROW.TELÉFONO;
+            DUI_ADMINISTRADOR.value = ROW.DUI;
+            NACIMIENTO_ADMINISTRADOR.value = ROW.NACIMIENTO;
+            CLAVE_ADMINISTRADOR.value = ROW.CLAVE;
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    } catch (Error) {
+        console.log(Error);
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar administrador';
+    }
+
+}
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el administrador de forma permanente?');
+    try {
+        // Se verifica la respuesta del mensaje.
+        if (RESPONSE) {
+            // Se define una constante tipo objeto con los datos del registro seleccionado.
+            const FORM = new FormData();
+            FORM.append('idAdministrador', id);
+            console.log(id);
+            // Petición para eliminar el registro seleccionado.
+            const DATA = await fetchData(ADMINISTRADOR_API, 'deleteRow', FORM);
+            console.log(DATA.status);
+            // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+            if (DATA.status) {
+                // Se muestra un mensaje de éxito.
+                await sweetAlert(1, DATA.message, true);
+                // Se carga nuevamente la tabla para visualizar los cambios.
+                cargarTabla();
+            } else {
+                sweetAlert(2, DATA.error, false);
+            }
+        }
+    }
+    catch (Error) {
+        console.log(Error + ' Error al cargar el mensaje');
+        confirmAction('¿Desea eliminar el administrador de forma permanente?');
+    }
+
+}
+
+
+async function cargarTabla(form = null) {
+    const lista_datos = [
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'José Martínez',
+            correo: 'jose@gmail.com',
+            telefono: '1234-5678',
+            dui: '12345678-9',
+            fecha: '1994-02-09',
+            id: 1,
+        },
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'José Martínez',
+            correo: 'jose@gmail.com',
+            telefono: '1234-5678',
+            dui: '12345678-9',
+            fecha: '1994-02-09',
+            id: 2,
+        },
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'José Martínez',
+            correo: 'jose@gmail.com',
+            telefono: '1234-5678',
+            dui: '12345678-9',
+            fecha: '1994-02-09',
+            id: 3,
+        },
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'José Martínez',
+            correo: 'jose@gmail.com',
+            telefono: '1234-5678',
+            dui: '12345678-9',
+            fecha: '1994-02-09',
+            id: 4,
+        }
+    ];
+    const cargarTabla = document.getElementById('tabla_administradores');
+
+    try {
+        cargarTabla.innerHTML = '';
+        // Se verifica la acción a realizar.
+        (form) ? action = 'searchRows' : action = 'readAll';
+        console.log(form);
+        // Petición para obtener los registros disponibles.
+        const DATA = await fetchData(ADMINISTRADOR_API, action, form);
+        console.log(DATA);
+
+        if (DATA.status) {
+            // Mostrar elementos obtenidos de la API
+            DATA.dataset.forEach(row => {
+                const tablaHtml = `
+                <tr>
+                    <td><img src="${SERVER_URL}images/admin/${row.IMAGEN}" height="50" width="50" class="circulo"></td>
+                    <td>${row.NOMBRE}</td>
+                    <td>${row.CORREO}</td>
+                    <td>${row.TELÉFONO}</td>
+                    <td>${row.DUI}</td>
+                    <td>${row.NACIMIENTO}</td>
+                    <td>
+                        <button type="button" class="btn btn-outline-success" onclick="openUpdate(${row.ID})">
+                        <img src="../../recursos/img/svg/icons_forms/pen 1.svg" width="30" height="30">
+                        </button>
+                        <button type="button" class="btn btn-outline-danger" onclick="openDelete(${row.ID})">
+                            <i class="bi bi-trash-fill"></i>
+                        </button>
+                    </td>
+                </tr>
+                `;
+                cargarTabla.innerHTML += tablaHtml;
+            });
+        } else {
+            sweetAlert(4, DATA.error, true);
+        }
+    } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+        // Mostrar materiales de respaldo
+        lista_datos.forEach(row => {
+            const tablaHtml = `
+            <tr>
+                <td><img src="${row.imagen}" height="50" width="50" class="circulo"></td>
+                <td>${row.nombre}</td>
+                <td>${row.correo}</td>
+                <td>${row.telefono}</td>
+                <td>${row.dui}</td>
+                <td>${row.fecha}</td>
+                <td>
+                    <button type="button" class="btn transparente" onclick="openUpdate(${row.id})">
+                    <img src="../../../resources/img/svg/icons_forms/pen 1.svg" width="18" height="18">
+                    </button>
+                    <button type="button" class="btn transparente" onclick="openDelete(${row.id})">
+                    <img src="../../../resources/img/svg/icons_forms/trash 1.svg" width="18" height="18">
+                    </button>
+                </td>
+            </tr>
+            `;
+            cargarTabla.innerHTML += tablaHtml;
+        });
+    }
+}
+
+// window.onload
+window.onload = async function () {
+    // Obtiene el contenedor principal
+    const appContainer = document.getElementById('main');
+    // Carga los componentes de manera síncrona
+    const adminHtml = await loadComponent('../componentes/admin.html');
+    // Llamada a la función para mostrar el encabezado.
+    loadTemplate();
+    // Agrega el HTML del encabezado
+    appContainer.innerHTML = adminHtml;
+    cargarTabla();
+    // Constantes para establecer los elementos del componente Modal.
+    SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+        MODAL_TITLE = document.getElementById('modalTitle');
+
+    // Constantes para establecer los elementos del formulario de guardar.
+    SAVE_FORM = document.getElementById('saveForm'),
+        ID_ADMINISTRADOR = document.getElementById('idAdministrador'),
+        NOMBRE_ADMINISTRADOR = document.getElementById('nombreAdministrador'),
+        APELLIDO_ADMINISTRADOR = document.getElementById('apellidoAdministrador'),
+        CORREO_ADMINISTRADOR = document.getElementById('correoAdministrador'),
+        TELEFONO_ADMINISTRADOR = document.getElementById('telefonoAdministrador'),
+        DUI_ADMINISTRADOR = document.getElementById('duiAdministrador'),
+        NACIMIENTO_ADMINISTRADOR = document.getElementById('nacimientoAdministrador'),
+        CLAVE_ADMINISTRADOR = document.getElementById('claveAdministrador'),
+        REPETIR_CLAVE = document.getElementById('repetirclaveAdministrador'),
+        ROL_ADMINISTRADOR = document.getElementById('rolAdministrador'),
+        IMAGEN_ADMINISTRADOR = document.getElementById('imagenAdministrador');
+    // Método del evento para cuando se envía el formulario de guardar.
+    SAVE_FORM.addEventListener('submit', async (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Se verifica la acción a realizar.
+        (ID_ADMINISTRADOR.value) ? action = 'updateRow' : action = 'createRow';
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SAVE_FORM);
+        // Petición para guardar los datos del formulario.
+        const DATA = await fetchData(ADMINISTRADOR_API, action, FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se cierra la caja de diálogo.
+            SAVE_MODAL.hide();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            cargarTabla();
+        } else {
+            sweetAlert(2, DATA.error, false);
+            console.error(DATA.exception);
+        }
+    });
+    // Constante para establecer el formulario de buscar.
+    SEARCH_FORM = document.getElementById('searchForm');
+    // Verificar si SEARCH_FORM está seleccionado correctamente
+    console.log(SEARCH_FORM)
+    // Método del evento para cuando se envía el formulario de buscar.
+    SEARCH_FORM.addEventListener('submit', (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SEARCH_FORM);
+        console.log(SEARCH_FORM);
+        console.log(FORM);
+        // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+        cargarTabla(FORM);
+    });
+    // Llamada a la función para establecer la mascara del campo teléfono.
+    vanillaTextMask.maskInput({
+        inputElement: document.getElementById('telefonoAdministrador'),
+        mask: [/\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
+    });
+    // Llamada a la función para establecer la mascara del campo DUI.
+    vanillaTextMask.maskInput({
+        inputElement: document.getElementById('duiAdministrador'),
+        mask: [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/]
+    });
+};
---
+++ controllers/utils/admin.js
@@ -0,0 +1,172 @@
+/*
+* Controlador de uso general en las páginas web del sitio privado.
+* Sirve para manejar la plantilla del encabezado y pie del documento.
+*/
+
+// Constante para completar la ruta de la API.
+const USER_API = 'services/admin/administrador.php';
+// Constante para establecer el elemento del contenido principal.
+const MAIN = document.querySelector('main');
+MAIN.style.paddingTop = '75px';
+MAIN.style.paddingBottom = '100px';
+MAIN.classList.add('container');
+
+/* Función asíncrona para cargar el encabezado y pie del documento.
+* Parámetros: ninguno.
+* Retorno: ninguno.
+*/
+const loadTemplate = async () => {
+
+// Se agrega el encabezado de la página web antes del contenido principal.
+MAIN.insertAdjacentHTML('beforebegin', `
+<header>
+    <nav class="navbar bg-skyBlue-pastel-color fixed-top sticky-sm-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Administrador</a>
+            <!-- Imagen de perfil y nombre -->
+            <div class="row align-items-center justify-content-center">
+                <div class="col-auto ">
+                    <div>
+                        <p class="mb-0 float-end">Chepe Martínez</p>
+                    </div>
+                    <div>
+                        <p class="text-body-secondary small float-end">Administrador</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <img src="../../../resources/img/svg/avatar.svg" class="rounded-circle" alt="">
+                </div>
+            </div>
+            <!-- Cuerpo -->
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                </div>
+                <!-- Cuerpo del nav -->
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <!-- Fila que contiene el logo centrado en la parte superior del menu -->
+                        <div class="row justify-content-center mb-5">
+                            <div class="col-auto">
+                                <img src="../../../resources/img/svg/logos/logo_blanco.svg" width="140px" alt="">
+                            </div>
+                        </div>
+                        <div class="container ms-2">
+                            <!-- Etiqueta para el dashboard -->
+                            <li class="nav-item">
+                                <a class="nav-link active text-light" aria-current="page" href="#">
+                                    <img src="../../../resources/img/svg/icons_menu/dashboard.svg" class="me-3"
+                                        alt="">Dashboard</a>
+                            </li>
+                            <!-- Etiqueta para el usuario -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../../../resources/img/svg/icons_menu/users.svg" class="me-3" alt="">
+                                    Usuarios
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                </ul>
+                            </li>
+                            <!-- Etiqueta para el estadisticas -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../../../resources/img/svg/icons_menu/estadisticas.svg" class="me-3" alt="">
+                                    Estadísticas
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                </ul>
+                            </li>
+                            <!-- Etiqueta para el entrenamientos -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../../../resources/img/svg/icons_menu/entrenamientos.svg" class="me-3"
+                                        alt="">
+                                    Entrenamientos
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                </ul>
+                            </li>
+                            <!-- Etiqueta para el equipos -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../../../resources/img/svg/icons_menu/equipos.svg" class="me-3" alt="">
+                                    Equipos
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                </ul>
+                            </li>
+                            <!-- Etiqueta para el registro medico -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../../../resources/img/svg/icons_menu/registro_medico.svg" class="me-3"
+                                        alt="">
+                                    Registro médico
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                </ul>
+                            </li>
+                            <!-- Etiqueta para pagos -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle text-light" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src="../../../resources/img/svg/icons_menu/pay.svg" class="me-3" alt="">
+                                    Pagos
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                </ul>
+                            </li>
+
+                            <div class="contenedor p-3 rounded-3">
+                                <!-- Etiqueta para el Ayuda -->
+                                <li class="nav-item">
+                                    <a class="nav-link active text-light" aria-current="page" href="#">
+                                        <img src="../../../resources/img/svg/icons_menu/help.svg" class="me-3" alt="">
+                                        Ayuda</a>
+                                </li>
+                                <!-- Etiqueta para contactanos -->
+                                <li class="nav-item">
+                                    <a class="nav-link active text-light" aria-current="page" href="#">
+                                        <img src="../../../resources/img/svg/icons_menu/contactanos.svg" class="me-3"
+                                            alt="">
+                                        contáctanos</a>
+                                </li>
+                                <!-- Etiqueta para log out -->
+                                <li class="nav-item">
+                                    <a class="nav-link active text-light" aria-current="page" href="#">
+                                        <img src="../../../resources/img/svg/icons_menu/log_out.svg" class="me-3" alt="">
+                                        Cerrar sesión</a>
+                                </li>
+                            </div>
+                        </div>
+                </div>
+                </ul>
+            </div>
+        </div>
+        </div>
+    </nav>
+</header>
+`);
+}
