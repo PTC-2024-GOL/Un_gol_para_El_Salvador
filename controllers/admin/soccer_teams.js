3c2d7374e7e43e09fe1a010e5b787db45a1cf211
Susan Castillo <susancasoli@gmail.com>
2024-04-03 11:29:04 -0600

    Pantalla de equipos finalizada

---
+++ controllers/admin/soccer_teams.js
@@ -0,0 +1,286 @@
+let SAVE_MODAL;
+let SEE_MODAL;
+let SAVE_FORM,
+    ID_EQUIPO,
+    NOMBRE_EQUIPO,
+    TELEFONO_EQUIPO,
+    ID_CUERPO_TECNICO,
+    ID_ADMINISTRADOR,
+    ID_CATEGORIA,
+    LOGO_EQUIPO
+let SEARCH_FORM;
+
+// Constantes para completar las rutas de la API.
+const EQUIPO_API = '';
+
+async function loadComponent(path) {
+    const response = await fetch(path);
+    const text = await response.text();
+    return text;
+}
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openCreate = () => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Crear equipo';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+}
+
+// Funcion para preparar el formulario al momento de abrirlo
+
+const seeModal = () => {
+    SEE_MODAL.show();
+    MODAL_TITLE.textContent = 'Cuerpo Técnico del equipo';
+    SAVE_FORM.reset();
+}
+
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    try {
+        // Se define un objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idEquipo', id);
+        // Petición para obtener los datos del registro solicitado.
+        const DATA = await fetchData(EQUIPO_API, 'readOne', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra la caja de diálogo con su título.
+            SAVE_MODAL.show();
+            MODAL_TITLE.textContent = 'Actualizar equipo';
+            // Se prepara el formulario.
+            SAVE_FORM.reset();
+            // Se inicializan los campos con los datos.
+            const ROW = DATA.dataset;
+            ID_EQUIPO.value = ROW.ID;
+            NOMBRE_EQUIPO.value = ROW.NOMBRE;
+            TELEFONO_EQUIPO.value = ROW.TELEFONO;
+            ID_CUERPO_TECNICO.value = ROW.ID_CUERPO_TECNICO;
+            ID_ADMINISTRADOR.value = ROW.ID_ADMINISTRADOR;
+            ID_CATEGORIA.value = ROW.ID_CATEGORIA;
+            LOGO_EQUIPO.value = ROW.LOGO;
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    } catch (Error) {
+        console.log(Error);
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar equipo';
+    }
+
+}
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar el equipo?');
+    try {
+        // Se verifica la respuesta del mensaje.
+        if (RESPONSE) {
+            // Se define una constante tipo objeto con los datos del registro seleccionado.
+            const FORM = new FormData();
+            FORM.append('idEquipo', id);
+            console.log(id);
+            // Petición para eliminar el registro seleccionado.
+            const DATA = await fetchData(EQUIPO_API_API, 'deleteRow', FORM);
+            console.log(DATA.status);
+            // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+            if (DATA.status) {
+                // Se muestra un mensaje de éxito.
+                await sweetAlert(1, DATA.message, true);
+                // Se carga nuevamente la tabla para visualizar los cambios.
+                cargarTabla();
+            } else {
+                sweetAlert(2, DATA.error, false);
+            }
+        }
+    }
+    catch (Error) {
+        console.log(Error + ' Error al cargar el mensaje');
+        confirmAction('¿Desea eliminar el equipo?');
+    }
+
+}
+
+
+async function cargarTabla(form = null) {
+    const lista_datos = [
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'Gol',
+            telefono: '2243-2312',
+            categoria: '16',
+            cuerpo_técnico: 1,
+            id: 1,
+        },
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'Monaco',
+            telefono: '1234-5678',
+            categoria: '17',
+            cuerpo_técnico: 1,
+            id: 2,
+        },
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'Selecta',
+            telefono: '1234-5678',
+            categoria: '28',
+            cuerpo_técnico: 1,
+            id: 3,
+        },
+        {
+            imagen: '../../../../resources/img/svg/avatar.svg',
+            nombre: 'Software',
+            telefono: '1234-5678',
+            categoria: '17',
+            cuerpo_técnico: 1,
+            id: 4,
+        }
+    ];
+    const cargarTabla = document.getElementById('tabla_equipos');
+
+    try {
+        cargarTabla.innerHTML = '';
+        // Se verifica la acción a realizar.
+        (form) ? action = 'searchRows' : action = 'readAll';
+        console.log(form);
+        // Petición para obtener los registros disponibles.
+        const DATA = await fetchData(EQUIPO_API_API, action, form);
+        console.log(DATA);
+
+        if (DATA.status) {
+            // Mostrar elementos obtenidos de la API
+            DATA.dataset.forEach(row => {
+                const tablaHtml = `
+                <tr>
+                    <td><img src="${SERVER_URL}images/admin/${row.IMAGEN}" height="50" width="50" class="circulo"></td>
+                    <td>${row.NOMBRE}</td>
+                    <td>${row.TELEFONO}</td>
+                    <td>${row.ID_CATEGORIA}</td>
+                    <td>
+                        <button type="button" class="btn btn-warnig" onclick="seeModal(${row.ID_CUERPO_TECNICO})">
+                        <img src="../../../resources/img/svg/icons_forms/cuerpo_tecnico.svg" width="30" height="30">
+                        </button>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-outline-success" onclick="openUpdate(${row.ID})">
+                        <img src="../../../recursos/img/svg/icons_forms/pen 1.svg" width="30" height="30">
+                        </button>
+                        <button type="button" class="btn btn-outline-danger" onclick="openDelete(${row.ID})">
+                            <i class="bi bi-trash-fill"></i>
+                        </button>
+                    </td>
+                </tr>
+                `;
+                cargarTabla.innerHTML += tablaHtml;
+            });
+        } else {
+            sweetAlert(4, DATA.error, true);
+        }
+    } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+        // Mostrar materiales de respaldo
+        lista_datos.forEach(row => {
+            const tablaHtml = `
+            <tr>
+                <td><img src="${row.imagen}" height="50" width="50" class="circulo"></td>
+                <td>${row.nombre}</td>
+                <td>${row.telefono}</td>
+                <td>${row.categoria}</td>
+                <td>
+                    <button type="button" class="btn transparente" onclick="seeModal(${row.cuerpo_técnico})">
+                    <img src="../../../resources/img/svg/icons_forms/cuerpo_tecnico.svg" width="18px" height="18px">
+                    </button>
+                </td>
+                <td>
+                    <button type="button" class="btn transparente" onclick="openUpdate(${row.id})">
+                    <img src="../../../resources/img/svg/icons_forms/pen 1.svg" width="18" height="18">
+                    </button>
+                    <button type="button" class="btn transparente" onclick="openDelete(${row.id})">
+                    <img src="../../../resources/img/svg/icons_forms/trash 1.svg" width="18" height="18">
+                    </button>
+                </td>
+            </tr>
+            `;
+            cargarTabla.innerHTML += tablaHtml;
+        });
+    }
+}
+
+// window.onload
+window.onload = async function () {
+    // Obtiene el contenedor principal
+    const appContainer = document.getElementById('main');
+    // Carga los componentes de manera síncrona
+    const equiposHtml = await loadComponent('../componentes/soccer_team.html');
+    // Llamada a la función para mostrar el encabezado.
+    loadTemplate();
+    // Agrega el HTML del encabezado
+    appContainer.innerHTML = equiposHtml;
+    cargarTabla();
+    // Constantes para establecer los elementos del componente Modal.
+    SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+        MODAL_TITLE = document.getElementById('modalTitle');
+
+    SEE_MODAL = new bootstrap.Modal('#seeModal'),
+        MODAL_TITLE = document.getElementById('modalTitle2')
+
+    // Constantes para establecer los elementos del formulario de guardar.
+    SAVE_FORM = document.getElementById('saveForm'),
+        ID_ADMINISTRADOR = document.getElementById('idEquipo'),
+        NOMBRE_EQUIPO = document.getElementById('nombreEquipo'),
+        TELEFONO_EQUIPO = document.getElementById('telefonoEquipo'),
+        ID_CATEGORIA = document.getElementById('categoriaEquipo'),
+        ID_CUERPO_TECNICO = document.getElementById('cuerpoTecnico'),
+    // Método del evento para cuando se envía el formulario de guardar.
+    SAVE_FORM.addEventListener('submit', async (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Se verifica la acción a realizar.
+        (ID_EQUIPO.value) ? action = 'updateRow' : action = 'createRow';
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SAVE_FORM);
+        // Petición para guardar los datos del formulario.
+        const DATA = await fetchData(EQUIPO_API_API, action, FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se cierra la caja de diálogo.
+            SAVE_MODAL.hide();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            cargarTabla();
+        } else {
+            sweetAlert(2, DATA.error, false);
+            console.error(DATA.exception);
+        }
+    });
+    // Constante para establecer el formulario de buscar.
+    SEARCH_FORM = document.getElementById('searchForm');
+    // Verificar si SEARCH_FORM está seleccionado correctamente
+    console.log(SEARCH_FORM)
+    // Método del evento para cuando se envía el formulario de buscar.
+    SEARCH_FORM.addEventListener('submit', (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SEARCH_FORM);
+        console.log(SEARCH_FORM);
+        console.log(FORM);
+        // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+        cargarTabla(FORM);
+    });
+};
