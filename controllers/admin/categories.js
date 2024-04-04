b3aa70fcf7ffc3abf06707468a2c8702f8224e16
sochiiii <20220018@ricaldone.edu.sv>
2024-04-03 22:05:17 -0600

    Creación de página Categorías

---
+++ controllers/admin/categories.js
@@ -0,0 +1,263 @@
+let SAVE_MODAL;
+let SAVE_FORM,
+    ID_CATEGORIAS,
+    NOMBRE_CATEGORIA,
+    EDAD_MIN,
+    EDAD_MAX,
+    TEMPORADA,
+    HORARIO;
+let SEARCH_FORM;
+
+// Constantes para completar las rutas de la API.
+const API = '';
+
+async function loadComponent(path) {
+    const response = await fetch(path);
+    const text = await response.text();
+    return text;
+}
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const openCreate = () => {
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Agregar una categoría';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+}
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openUpdate = async (id) => {
+    try {
+        // Se define un objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('idCategoria', id);
+        // Petición para obtener los datos del registro solicitado.
+        const DATA = await fetchData(API, 'readOne', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se muestra la caja de diálogo con su título.
+            SAVE_MODAL.show();
+            MODAL_TITLE.textContent = 'Actualizar una categoría';
+            // Se prepara el formulario.
+            SAVE_FORM.reset();
+            // Se inicializan los campos con los datos.
+            const ROW = DATA.dataset;
+            ID_CATEGORIAS.value = ROW.ID;
+            NOMBRE_CATEGORIA.value = ROW.NOMBRE;
+            EDAD_MIN.value = ROW.MINIMA;
+            EDAD_MAX.value = ROW.MAXIMA;
+            TEMPORADA.value = ROW.TEMPORADA;
+            HORARIO.value = ROW.HORARIO;
+        } else {
+            sweetAlert(2, DATA.error, false);
+        }
+    } catch (Error) {
+        console.log(Error);
+        SAVE_MODAL.show();
+        MODAL_TITLE.textContent = 'Actualizar una categoría';
+    }
+
+}
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+const openDelete = async (id) => {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea eliminar la categoría?');
+    try {
+        // Se verifica la respuesta del mensaje.
+        if (RESPONSE) {
+            // Se define una constante tipo objeto con los datos del registro seleccionado.
+            const FORM = new FormData();
+            FORM.append('idCategoria', id);
+            console.log(id);
+            // Petición para eliminar el registro seleccionado.
+            const DATA = await fetchData(API, 'deleteRow', FORM);
+            console.log(DATA.status);
+            // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+            if (DATA.status) {
+                // Se muestra un mensaje de éxito.
+                await sweetAlert(1, DATA.message, true);
+                // Se carga nuevamente la tabla para visualizar los cambios.
+                fillTable();
+            } else {
+                sweetAlert(2, DATA.error, false);
+            }
+        }
+    }
+    catch (Error) {
+        console.log(Error + ' Error al cargar el mensaje');
+        confirmAction('¿Desea eliminar la categoría');
+    }
+
+}
+
+
+async function fillTable(form = null) {
+    const lista_datos = [
+        {
+            nombre: 'Nivel 1',
+            minima: '4',
+            maxima: '7',
+            temporada: '2021',
+            horario: '14:00:00',
+            id: 1,
+        },
+        {
+            nombre: 'Nivel 2',
+            minima: '8',
+            maxima: '13',
+            temporada: '2022',
+            horario: '16:00:00',
+            id: 2,
+        },
+        {
+            nombre: 'Nivel 3',
+            minima: '14',
+            maxima: '15',
+            temporada: '2023',
+            horario: '15:00:00',
+            id: 3,
+        },
+        {
+            nombre: 'Nivel 4',
+            minima: '16',
+            maxima: '18',
+            temporada: '2024',
+            horario: '14:00:00',
+            id: 4,
+        }
+    ];
+    const cargarTabla = document.getElementById('tabla_categorias');
+
+    try {
+        cargarTabla.innerHTML = '';
+        // Se verifica la acción a realizar.
+        (form) ? action = 'searchRows' : action = 'readAll';
+        console.log(form);
+        // Petición para obtener los registros disponibles.
+        const DATA = await fetchData(API, action, form);
+        console.log(DATA);
+
+        if (DATA.status) {
+            // Mostrar elementos obtenidos de la API
+            DATA.dataset.forEach(row => {
+                const tablaHtml = `
+                <tr>
+                    <td>${row.NOMBRE}</td>
+                    <td>${row.MINIMA}</td>
+                    <td>${row.MAXIMA}</td>
+                    <td>${row.TEMPORADA}</td>
+                    <td>${row.HORARIO}</td>
+                    <td>
+                        <button type="button" class="btn btn-outline-success" onclick="openUpdate(${row.ID})">
+                        <img src="../../recursos/img/svg/icons_forms/pen 1.svg" width="30" height="30">
+                        </button>
+                        <button type="button" class="btn btn-outline-danger" onclick="openDelete(${row.ID})">
+                            <i class="bi bi-trash-fill"></i>
+                        </button>
+                    </td>
+                </tr>
+                `;
+                cargarTabla.innerHTML += tablaHtml;
+            });
+        } else {
+            sweetAlert(4, DATA.error, true);
+        }
+    } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+        // Mostrar materiales de respaldo
+        lista_datos.forEach(row => {
+            const tablaHtml = `
+            <tr>
+            <td>${row.nombre}</td>
+            <td>${row.minima}</td>
+            <td>${row.maxima}</td>
+            <td>${row.temporada}</td>
+            <td>${row.horario}</td>
+                    <td>
+                    <button type="button" class="btn transparente" onclick="openUpdate(${row.id})">
+                    <img src="../../../resources/img/svg/icons_forms/pen 1.svg" width="18" height="18">
+                    </button>
+                    <button type="button" class="btn transparente" onclick="openDelete(${row.id})">
+                    <img src="../../../resources/img/svg/icons_forms/trash 1.svg" width="18" height="18">
+                    </button>
+                    </td>
+                </tr>
+            `;
+            cargarTabla.innerHTML += tablaHtml;
+        });
+    }
+}
+
+// window.onload
+window.onload = async function () {
+    // Obtiene el contenedor principal
+    const appContainer = document.getElementById('main');
+    // Carga los componentes de manera síncrona
+    const horarioHtml = await loadComponent('../componentes/categories.html');
+    // Llamada a la función para mostrar el encabezado.
+    loadTemplate();
+    // Agrega el HTML del encabezado
+    appContainer.innerHTML = horarioHtml;
+    fillTable();
+    // Constantes para establecer los elementos del componente Modal.
+    SAVE_MODAL = new bootstrap.Modal('#saveModal'),
+        MODAL_TITLE = document.getElementById('modalTitle');
+
+    // Constantes para establecer los elementos del formulario de guardar.
+    SAVE_FORM = document.getElementById('saveForm'),
+        ID_CATEGORIAS = document.getElementById('idCategoria'),
+        NOMBRE_CATEGORIA = document.getElementById('nombreCategoria'),
+        EDAD_MIN = document.getElementById('edadMin'),
+        EDAD_MAX = document.getElementById('edadMax'),
+        TEMPORADA = document.getElementById('temporada'),
+        HORARIO = document.getElementById('horario');
+    // Método del evento para cuando se envía el formulario de guardar.
+    SAVE_FORM.addEventListener('submit', async (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Se verifica la acción a realizar.
+        (ID_CATEGORIAS.value) ? action = 'updateRow' : action = 'createRow';
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SAVE_FORM);
+        // Petición para guardar los datos del formulario.
+        const DATA = await fetchData(API, action, FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se cierra la caja de diálogo.
+            SAVE_MODAL.hide();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, DATA.message, true);
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+        } else {
+            sweetAlert(2, DATA.error, false);
+            console.error(DATA.exception);
+        }
+    });
+    // Constante para establecer el formulario de buscar.
+    SEARCH_FORM = document.getElementById('searchForm');
+    // Verificar si SEARCH_FORM está seleccionado correctamente
+    console.log(SEARCH_FORM)
+    // Método del evento para cuando se envía el formulario de buscar.
+    SEARCH_FORM.addEventListener('submit', (event) => {
+        // Se evita recargar la página web después de enviar el formulario.
+        event.preventDefault();
+        // Constante tipo objeto con los datos del formulario.
+        const FORM = new FormData(SEARCH_FORM);
+        console.log(SEARCH_FORM);
+        console.log(FORM);
+        // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+        fillTable(FORM);
+    });
+};
--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -125,7 +125,7 @@ MAIN.insertAdjacentHTML('beforebegin', `
                                     <li><a class="dropdown-item text-light" href="#">Tipos de goles</a></li>
                                     <li><a class="dropdown-item text-light" href="#">Participaciones</a></li>
                                     <li><a class="dropdown-item text-light" href="positions.html">Posiciones</a></li>
-                                    <li><a class="dropdown-item text-light" href="#">Categorías</a></li>
+                                    <li><a class="dropdown-item text-light" href="categories.html">Categorías</a></li>
                                     <li><a class="dropdown-item text-light" href="#">Partidos</a></li>
                                 </ul>
                             </li>
