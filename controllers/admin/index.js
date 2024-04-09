d623d31b576cb36b0c8cbe1dd39564871040b9d1
Torugam <joelomarmena@gmail.com>
2024-04-09 15:44:16 -0600

    En progreso pantalla index

---
+++ controllers/admin/index.js
@@ -0,0 +1,15 @@
+async function loadComponent(path) {
+    const response = await fetch(path);
+    const text = await response.text();
+    return text;
+}
+
+window.onload = async function () {
+    // Obtiene el contenedor principal
+    const appContainer = document.getElementById('main');
+    // Carga los componentes de manera síncrona
+    const adminHtml = await loadComponent('../componentes/index.html');
+
+    appContainer.innerHTML = adminHtml;
+    
+};
