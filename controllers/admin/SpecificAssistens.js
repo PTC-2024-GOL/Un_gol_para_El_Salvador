d5a7d633d496333c1043ca65a02ca58a71c9fc7f
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-04-14 13:09:15 -0600

    SpecificAssitents terminado
    Aún no he definido la lógica a la perfección pero el diseño ya está

--- controllers/admin/SpecificAssistens.js
+++ controllers/admin/SpecificAssistens.js
@@ -8,20 +8,80 @@ let SAVE_FORM,
 let SEARCH_FORM;
 
 // Constantes para completar las rutas de la API.
-const API = '';
+// La lógica de esta pantalla aún no está terminada debido a que todavía me hace falta definir qué ocurrirá cuando se agregue una observación Y cuando se seleccione Una nueva opción de los ítems que contiene cada select
+// Por lo tanto guayito del futuro, Tienes la tarea de hacer que el botón de guardar asistencias capture el json de lista datos Y active la opción de post.
+// Debes verificar que cuando se seleccione una opción se actualiza el jason y que cuando se agregue una nueva observación se actualice el json tambien
+const ASISTENCIAS_API_2 = '';
+
+const lista_datos_asistencias = [
+    {
+        asistencia: "Asistencia"
+    },
+    {
+        asistencia: "Ausencia injustificada"
+    },
+    {
+        asistencia: "Enfermedad"
+    },
+    {
+        asistencia: "Estudio"
+    },
+    {
+        asistencia: "Trabajo"
+    },
+    {
+        asistencia: "Viaje"
+    },
+    {
+        asistencia: "Permiso"
+    },
+    {
+        asistencia: "Falta"
+    },
+    {
+        asistencia: "Lesión"
+    },
+    
+    {
+        asistencia: "Otro"
+    }
+];
 
 async function loadComponent(path) {
     const response = await fetch(path);
     const text = await response.text();
     return text;
 }
-/*
-*   Función para preparar el formulario al momento de insertar un registro.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
+
+// Función para poblar un combobox (select) con opciones
+async function fillSelected(data, action, selectElement, selectedValue = '') {
+    try {
+        
+        // Llenar el combobox con los datos proporcionados
+        data.forEach(item => {
+            const option = document.createElement('option');
+            option.value = item.asistencia; // Suponiendo que hay una propiedad 'asistencia' en los datos y la queremos usar como identificador
+            option.textContent = item.asistencia; // Cambia 'asistencia' al nombre de la propiedad que deseas mostrar en el combobox
+            selectElement.appendChild(option);
+        });
+        // Seleccionar el valor especificado si se proporciona
+        if (selectedValue !== '') {
+            selectElement.value = selectedValue;
+        }
+    } catch (error) {
+        console.error('Error al llenar el combobox:', error);
+    }
+}
+
 const openCreate = () => {
-    //Se muestra una caja de dialogo de que se guardaron exitosamente los registros
+    // Se muestra la caja de diálogo con su título.
+    SAVE_MODAL.show();
+    MODAL_TITLE.textContent = 'Agregar observación';
+    // Se prepara el formulario.
+    SAVE_FORM.reset();
+    // Se inicializan los campos con los datos.
+    ID_ASISTENCIA.value = '';
+    OBSERVACION.value = '';
 }
 /*
 *   Función asíncrona para preparar el formulario al momento de actualizar un registro.
@@ -34,7 +94,7 @@ const openUpdate = async (id) => {
         const FORM = new FormData();
         FORM.append('idAsitencia', id);
         // Petición para obtener los datos del registro solicitado.
-        const DATA = await fetchData(ADMINISTRADOR_API, 'readOne', FORM);
+        const DATA = await fetchData(ASISTENCIAS_API_2, 'readOne', FORM);
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (DATA.status) {
             // Se muestra la caja de diálogo con su título.
@@ -56,6 +116,21 @@ const openUpdate = async (id) => {
     }
 
 }
+// Crea una función que en base a un id, cree un combobox y usando la función fillSelected, lo llene con las opciones de la lista_datos_asistencias y que se seleccione la opción por defecto llamada Asistencia, quiero tener control de cada combobox así que cuando le crees su id, usa el id que se le pase como parametro como identificador de cada combobox.
+async function createSelect(identificador) {
+    try {
+        const selectElement = document.createElement('select');
+        newid = 'asistencia_' + identificador;
+        selectElement.id = newid;
+        await fillSelected(lista_datos_asistencias, 'readAll', selectElement, 'Asistencia');
+        console.log('Elemento select antes de devolver:', selectElement);
+        selectElement.classList.add('form-select');
+        return selectElement;
+    } catch (error) {
+        console.error('Error al crear el combobox:', error);
+    }
+}
+
 
 
 
@@ -63,23 +138,27 @@ async function fillTable(form = null) {
     const lista_datos = [
         {
             jugador: 'Antonio Morales',
+            asistencia: 'Asistencia',
             observacion: 'bla blab bla',
-            id: 0,
+            id: 1,
         },
         {
             jugador: 'Jose Antonio',
+            asistencia: 'Asistencia',
             observacion: 'bla blab bla',
-            id: 0,
+            id: 2,
         },
         {
             jugador: 'Menchoga Pastre ',
+            asistencia: 'Asistencia',
             observacion: 'bla blab bla',
-            id: 0,
+            id: 3,
         },
         {
             jugador: 'Sanchez Cerén',
+            asistencia: 'Asistencia',
             observacion: 'bla blab bla',
-            id: 2,
+            id: 4,
         }
     ];
     const cargarTabla = document.getElementById('tabla_asistencias_2');
@@ -87,10 +166,10 @@ async function fillTable(form = null) {
     try {
         cargarTabla.innerHTML = '';
         // Se verifica la acción a realizar.
-        (form) ? action = 'searchRows' : action = 'readAll';
+        const action = (form) ? 'searchRows' : 'readAll';
         console.log(form);
         // Petición para obtener los registros disponibles.
-        const DATA = await fetchData(API, action, form);
+        const DATA = await fetchData(ASISTENCIAS_API_2, action, form);
         console.log(DATA);
 
         if (DATA.status) {
@@ -99,6 +178,7 @@ async function fillTable(form = null) {
                 const tablaHtml = `
                 <tr>
                     <td>${row.JUGADOR}</td>
+                    <td> ${createSelect(row.ID).outerHTML}</td>
                     <td>
                         <button type="button" class="btn btn-warnig" onclick="openUpdate(${row.ID})">
                         <img src="../../../resources/img/svg/icons_forms/cuerpo_tecnico.svg" width="30" height="30">
@@ -114,21 +194,37 @@ async function fillTable(form = null) {
     } catch (error) {
         console.error('Error al obtener datos de la API:', error);
         // Mostrar materiales de respaldo
-        lista_datos.forEach(row => {
-            const tablaHtml = `
-            <tr>
-                    <td>${row.jugador}</td>
-                    <td>
-                        <button type="button" class="btn btn-warnig" onclick="openUpdate(${row.id})">
-                        <img src="../../../resources/img/svg/icons_forms/cuerpo_tecnico.svg" width="30" height="30">
-                        </button>
-                    </td>
-                </tr>
-            `;
-            cargarTabla.innerHTML += tablaHtml;
-        });
+        if (lista_datos.length > 0) {
+            console.error('Entre al if, así que si detecto que hay datos');
+            
+            // Primero, creamos todas las promesas para los elementos select
+            const selectPromises = lista_datos.map(row => createSelect(row.id));
+            
+            // Luego, resolvemos todas las promesas
+            Promise.all(selectPromises).then(selects => {
+                // Ahora que todas las promesas se han resuelto, podemos construir la cadena de texto
+                lista_datos.forEach((row, index) => {
+                    console.error('Estos son los id Encontrados: ', row.id);
+                    const tablaHtml = `
+                        <tr>
+                            <td>${row.jugador}</td>
+                            <td>${selects[index].outerHTML}</td>
+                            <td>
+                                <button type="button" class="btn btn-warnig" onclick="openUpdate(${row.id})">
+                                    <img src="../../../resources/img/svg/icons_forms/cuerpo_tecnico.svg" width="30" height="30">
+                                </button>
+                            </td>
+                        </tr>
+                    `;
+                    cargarTabla.innerHTML += tablaHtml;
+                });
+            });
+        } else {
+            cargarTabla.innerHTML = '<tr><td colspan="3">No se encontraron registros.</td></tr>';
+        }
     }
 }
+
 
 // window.onload
 window.onload = async function () {
@@ -161,7 +257,7 @@ window.onload = async function () {
         // Constante tipo objeto con los datos del formulario.
         const FORM = new FormData(SAVE_FORM);
         // Petición para guardar los datos del formulario.
-        const DATA = await fetchData(API, action, FORM);
+        const DATA = await fetchData(ASISTENCIAS_API_2, action, FORM);
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (DATA.status) {
             // Se cierra la caja de diálogo.
