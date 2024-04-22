c9911fb5fe90c1d006096def4ab99b93b62724f4
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-04-22 10:59:08 -0600

    Eliminación de mensaje de eliminar en plantillas de técnicos

--- controllers/technicals/templates.js
+++ controllers/technicals/templates.js
@@ -84,7 +84,6 @@ const openDelete = async (id) => {
     }
     catch (Error) {
         console.log(Error + ' Error al cargar el mensaje');
-        confirmAction('¿Desea eliminar la plantilla?');
     }
 
 }
