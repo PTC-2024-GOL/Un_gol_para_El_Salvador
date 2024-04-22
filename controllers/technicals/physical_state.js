e8e2b66f671d1716f33782a68c37473f8d51788c
Torugam <joelomarmena@gmail.com>
2024-04-22 11:03:03 -0600

    correción de js técnico - estado fisico

--- controllers/technicals/physical_state.js
+++ controllers/technicals/physical_state.js
@@ -92,7 +92,6 @@ const openDelete = async (id) => {
     }
     catch (Error) {
         console.log(Error + ' Error al cargar el mensaje');
-        confirmAction('¿Desea eliminar el registro?');
     }
 
 }
