a14628b41454fc54f0d7f5445fe4a6898a4c0ec5
sochiiii <20220018@ricaldone.edu.sv>
2024-04-22 10:50:05 -0600

    Eliminación de error de doble alerta

--- controllers/admin/admin.js
+++ controllers/admin/admin.js
@@ -102,7 +102,7 @@ const openDelete = async (id) => {
     }
     catch (Error) {
         console.log(Error + ' Error al cargar el mensaje');
-        confirmAction('¿Desea eliminar el administrador?');
+        
     }
 
 }
