6f3c89e62c816e32ed0bbe4e14da8e5872222d95
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-06-11 00:57:41 -0600

    Correcciones

--- controllers/admin/technical.js
+++ controllers/admin/technical.js
@@ -148,102 +148,97 @@ const openState = async (id) => {
 
 }
 
+const tecnicosPorPagina = 10;
+let paginaActual = 1;
+let tecnicos = [];
 
+// Función para cargar técnicos con paginación
 async function fillTable(form = null) {
-    const lista_datos = [
-        {
-            imagen: '../../../../resources/img/svg/avatar.svg',
-            nombre: 'José Martínez',
-            correo: 'jose@gmail.com',
-            telefono: '1234-5678',
-            dui: '12345678-9',
-            fecha: '1994-02-09',
-            id: 1,
-        },
-        {
-            imagen: '../../../../resources/img/svg/avatar2.svg',
-            nombre: 'Wilmer Melara',
-            correo: 'wilmer@gmail.com',
-            telefono: '1234-5678',
-            dui: '12345678-9',
-            fecha: '1994-02-09',
-            id: 2,
-        },
-    ];
     const cargarTabla = document.getElementById('tabla_tecnicos');
-
     try {
         cargarTabla.innerHTML = '';
-        // Se verifica la acción a realizar.
-        (form) ? action = 'searchRows' : action = 'readAll';
-        console.log(form);
         // Petición para obtener los registros disponibles.
+        let action;
+        form ? action = 'searchRows' : action = 'readAll';
+        console.log(form);
         const DATA = await fetchData(API, action, form);
         console.log(DATA);
 
         if (DATA.status) {
-            // Mostrar elementos obtenidos de la API
-            DATA.dataset.forEach(row => {
-                const tablaHtml = `
-                <tr class="${getRowBackgroundColor(row.ESTADO)}">
-                    <td><img src="${SERVER_URL}images/tecnicos/${row.IMAGEN}" height="50" width="50" class="circulo"></td>
-                    <td>${row.NOMBRE}</td>
-                    <td>${row.CORREO}</td>
-                    <td>${row.TELÉFONO}</td>
-                    <td>${row.DUI}</td>
-                    <td class="${getRowColor(row.ESTADO)}">${row.ESTADO}</td>
-                    <td>
-                    <button type="button" class="btn transparente" onclick="openState(${row.ID})">
-                    <img src="../../../resources/img/svg/icons_forms/amonestacion.svg" width="18" height="18">
-                    </button>
-                    <button type="button" class="btn transparente" onclick="openUpdate(${row.ID})">
-                    <img src="../../../resources/img/svg/icons_forms/pen 1.svg" width="18" height="18">
-                    </button>
-                    <button type="button" class="btn transparente" onclick="openDelete(${row.ID})">
-                    <img src="../../../resources/img/svg/icons_forms/trash 1.svg" width="18" height="18">
-                    </button>
-                    </td>
-                </tr>
-                `;
-                cargarTabla.innerHTML += tablaHtml;
-                // Se muestra un mensaje de acuerdo con el resultado.
-                ROWS_FOUND.textContent = DATA.message;
-            });
+            tecnicos = DATA.dataset;
+            mostrarTecnicos(paginaActual);
+            // Se muestra un mensaje de acuerdo con el resultado.
+            ROWS_FOUND.textContent = DATA.message;
         } else {
-            sweetAlert(4, DATA.error, true);
+            // Se muestra un mensaje de acuerdo con el resultado.
+            ROWS_FOUND.textContent = "Existen 0 coincidencias";
         }
     } catch (error) {
         console.error('Error al obtener datos de la API:', error);
-        // Mostrar materiales de respaldo
-        lista_datos.forEach(row => {
-            const tablaHtml = `
-            <tr>
-                <td><img src="${row.imagen}" height="50" width="50" class="circulo"></td>
-                <td>${row.nombre}</td>
-                <td>${row.correo}</td>
-                <td>${row.telefono}</td>
-                <td>${row.dui}</td>
-                <td>${row.fecha}</td>
-                <td>
-                    <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckCheckedDisabled" checked disabled>
-                    </div>
-                </td>
-                <td>
-                    <button type="button" class="btn transparente" onclick="openUpdate(${row.id})">
-                    <img src="../../../resources/img/svg/icons_forms/pen 1.svg" width="18" height="18">
-                    </button>
-                    <button type="button" class="btn transparente" onclick="openDelete(${row.id})">
-                    <img src="../../../resources/img/svg/icons_forms/trash 1.svg" width="18" height="18">
-                    </button>
-                </td>
-            </tr>
-            `;
-            cargarTabla.innerHTML += tablaHtml;
-        });
     }
 }
 
+// Función para mostrar técnicos en una página específica
+function mostrarTecnicos(pagina) {
+    const inicio = (pagina - 1) * tecnicosPorPagina;
+    const fin = inicio + tecnicosPorPagina;
+    const tecnicosPagina = tecnicos.slice(inicio, fin);
+
+    const cargarTabla = document.getElementById('tabla_tecnicos');
+    cargarTabla.innerHTML = '';
+    tecnicosPagina.forEach(row => {
+        const tablaHtml = `
+            <tr class="${getRowBackgroundColor(row.ESTADO)}">
+                <td><img src="${SERVER_URL}images/tecnicos/${row.IMAGEN}" height="50" width="50" class="circulo"></td>
+                <td>${row.NOMBRE}</td>
+                <td>${row.CORREO}</td>
+                <td>${row.TELÉFONO}</td>
+                <td>${row.DUI}</td>
+                <td class="${getRowColor(row.ESTADO)}">${row.ESTADO}</td>
+                <td>
+                    <button type="button" class="btn transparente" onclick="openState(${row.ID})">
+                        <img src="../../../resources/img/svg/icons_forms/amonestacion.svg" width="18" height="18">
+                    </button>
+                    <button type="button" class="btn transparente" onclick="openUpdate(${row.ID})">
+                        <img src="../../../resources/img/svg/icons_forms/pen 1.svg" width="18" height="18">
+                    </button>
+                    <button type="button" class="btn transparente" onclick="openDelete(${row.ID})">
+                        <img src="../../../resources/img/svg/icons_forms/trash 1.svg" width="18" height="18">
+                    </button>
+                </td>
+            </tr>
+        `;
+        cargarTabla.innerHTML += tablaHtml;
+    });
+
+    actualizarPaginacion();
+}
+
+// Función para actualizar los controles de paginación
+function actualizarPaginacion() {
+    const paginacion = document.querySelector('.pagination');
+    paginacion.innerHTML = '';
+
+    const totalPaginas = Math.ceil(tecnicos.length / tecnicosPorPagina);
+
+    if (paginaActual > 1) {
+        paginacion.innerHTML += `<li class="page-item"><a class="page-link text-dark" href="#" onclick="cambiarPagina(${paginaActual - 1})">Anterior</a></li>`;
+    }
+
+    for (let i = 1; i <= totalPaginas; i++) {
+        paginacion.innerHTML += `<li class="page-item ${i === paginaActual ? 'active' : ''}"><a class="page-link text-dark" href="#" onclick="cambiarPagina(${i})">${i}</a></li>`;
+    }
+
+    if (paginaActual < totalPaginas) {
+        paginacion.innerHTML += `<li class="page-item"><a class="page-link text-dark" href="#" onclick="cambiarPagina(${paginaActual + 1})">Siguiente</a></li>`;
+    }
+}
+
+// Función para cambiar de página
+function cambiarPagina(nuevaPagina) {
+    paginaActual = nuevaPagina;
+    mostrarTecnicos(paginaActual);
+}
 
 function getRowColor(estado) {
     switch (estado) {
@@ -354,7 +349,7 @@ window.onload = async function () {
         if (file) {
             const reader = new FileReader();
             reader.onload = function(e) {
-                FOTO_TECNICO.src = e.target.result;
+                FOTO_ADMINISTRADOR.src = e.target.result;
             }
             reader.readAsDataURL(file);
         }
