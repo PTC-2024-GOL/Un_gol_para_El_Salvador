da2ecaa5c8a83cbc722e4f83727efe88e3f7884a
JuanPabloFloresDiaz <20220599@ricaldone.edu.sv>
2024-07-19 18:05:06 -0600

    Validación del primer uso

--- controllers/admin/first_user.js
+++ controllers/admin/first_user.js
@@ -21,8 +21,6 @@ window.onload = async function () {
     // Agrega el HTML del encabezado
     appContainer.innerHTML = cambiarregistro;
 
-
-
     // Llamada a la función para establecer la mascara del campo teléfono.
     vanillaTextMask.maskInput({
         inputElement: document.getElementById('telefonoAdministrador'),
@@ -38,7 +36,6 @@ window.onload = async function () {
     SAVE_FORM = document.getElementById('saveForm'),
         FOTO = document.getElementById('img_admin'),
         IMAGEN = document.getElementById('imagenAdministrador');
-
 
     IMAGEN.addEventListener('change', function (event) {
         // Verifica si hay una imagen seleccionada
@@ -72,19 +69,27 @@ window.onload = async function () {
         SAVE_FORM.addEventListener('submit', async (event) => {
             // Se evita recargar la página web después de enviar el formulario.
             event.preventDefault();
-            // Constante tipo objeto con los datos del formulario.
-            const FORM = new FormData(SAVE_FORM);
-            // Petición para iniciar sesión.
-            const DATA = await fetchData(USER_API, 'signUp', FORM);
-            console.log(DATA);
-            // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-            if (DATA.status) {
-                sweetAlert(1, DATA.message, true, 'index.html');
+            // Petición para consultar los usuarios registrados.
+            const DATA = await fetchData(USER_API, 'readUsers');
+            // Se comprueba si existe una sesión, de lo contrario se sigue con el flujo normal.
+            if (DATA.session) {
+                // Se direcciona a la página web de bienvenida.
+                sweetAlert(4, DATA.error, true, 'dashboard.html');
+            } else if (DATA.status) {
+                sweetAlert(4, 'Ya existen administradores dentro del sistema', true, 'index.html');
             } else {
-                sweetAlert(2, DATA.error, false);
+                // Constante tipo objeto con los datos del formulario.
+                const FORM = new FormData(SAVE_FORM);
+                // Petición para iniciar sesión.
+                const DATA = await fetchData(USER_API, 'signUp', FORM);
+                console.log(DATA);
+                // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                if (DATA.status) {
+                    sweetAlert(1, DATA.message, true, 'index.html');
+                } else {
+                    sweetAlert(2, DATA.error, false);
+                }
             }
         });
     }
-
-
 };
