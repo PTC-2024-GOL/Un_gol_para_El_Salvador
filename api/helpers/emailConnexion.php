7245b578b2ea739110401a68926d0a0d5081b921
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-08-11 21:22:20 -0600

    Alertas corregidas en recovery pass

--- api/helpers/emailConnexion.php
+++ api/helpers/emailConnexion.php
@@ -1,81 +1,46 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
 require ('C:/xampp/htdocs/sitio_gol_sv/vendor/autoload.php');
 
-class Email
+class Props
 {
-    private static $encryptionKey = '6363637034303f4876'; // Clave en formato hexadecimal (32 bytes)
-
-    public static function sendMail($id_usuario, $nivel_usuario, $cadena, $correo, $nombre)
+    public static function sendMail($address, $subject, $message, $attachmentPath = null)
     {
-        $encryptedString = self::encryptString($cadena);
 
-        $address = $correo;
-        $subject = 'Recuperación de contraseña';
-        $name = $nombre;
-        $link = 'http://localhost/sitio_gol_sv/views/admin/pages/change_passwords.html?c=' . urlencode($cadena) . '&id=' . urlencode($id_usuario) . '&n=' . urlencode($nivel_usuario);
         $mail = new PHPMailer(true);
 
         try {
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = 'guayito.palom0@gmail.com';
-            $mail->Password = 'xokbehxzwqumgsvd';
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-            $mail->Port = 465;
 
-            $mail->setFrom($address);
-            $mail->addAddress('guayito.palom0@gmail.com', 'Soporte un gol para El Salvador');
-            $mail->addCC($address);
-            $mail->CharSet = 'UTF-8';
+            // Configuración del servidor
+
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Habilitar salida de depuración detallada
+            $mail->isSMTP();                                            // Enviar usando SMTP
+            $mail->Host       = 'smtp.gmail.com';                     // Configurar el servidor SMTP para enviar a través de Gmail
+            $mail->SMTPAuth   = true;                                   // Habilitar autenticación SMTP
+            $mail->Username = 'guayito.palom0@gmail.com';
+            $mail->Password = 'xokbehxzwqumgsvd';                             // Contraseña SMTP
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // Habilitar cifrado TLS implícito
+            $mail->Port       = 465;                                    // Puerto TCP para conectarse; usa 587 si has configurado `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            // Destinatarios
+            $mail->setFrom('guayito.palom0@gmail.com', 'Un gol para El salvador'); // Quien lo envía
+            $mail->addAddress($address);     // Agregar un destinatario
+            $mail->CharSet = 'UTF-8'; //caracteres especiales
             $mail->isHTML(true);
             $mail->Subject = $subject;
-            $mail->Body = "
-                <body style='font-family: Arial, sans-serif;'>
-                    <div style='width: 700px; margin: 0 auto;'>
-                        <div style='text-align: center'>
-                            <img src='https://images.vexels.com/content/218082/preview/male-soccer-player-passing-ball-d34c2e.png' style='width: 150px; height: 150px' alt=''>
-                        </div>
-                        <h2 style='background-color: #0078B7; color: white; margin-top: 0; text-align: center; padding: 10px'> ¡Bienvenido $name, estamos aquí para ayudarte!</h2>
-                        <hr style='border-top: 1px solid #ddd;'>
-                        <div style='line-height: 1.5; padding: 15px; background-color: #CAF1F8; font-size: 17px'>
-                            Cambia tu contraseña con un solo click, <a href='$link' style='color: #0078B7;'>¡ingresa aquí!</a>. Si no has solicitado este cambio, por favor ignora este mensaje.
-                            Este link solo estará habilitado durante los siguientes 15 minutos, después de este tiempo deberás solicitar un nuevo cambio de contraseña. Recuerda que tu seguridad es nuestra prioridad. 
-                        </div>
-                        <div style='text-align: center; padding: 10px; background-color: #0078B7;'>
-                            <p style='color: white; font-size: 15px'>Copyright &copy; " . date("Y") . " Fundación un gol para El Salvador. Todos los derechos reservados.</p>
-                        </div>
-                    </div>
-                </body>
-            ";
+            $mail->Body = $message;
+            // Añadir adjunto
+            if ($attachmentPath) {
+                $mail->addAttachment($attachmentPath);
+            }
 
             $mail->send();
             return 1;
         } catch (Exception $e) {
+
             return 0;
         }
     }
-
-    private static function encryptString($string)
-    {
-        $key = hex2bin(self::$encryptionKey); // Convertir clave de hexadecimal a binario
-        $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc')); // Generar IV aleatorio
-        $encrypted = openssl_encrypt($string, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
-        return base64_encode($iv . $encrypted); // Concatenar IV y texto cifrado, luego codificar en Base64
-    }
-
-    private static function decryptString($string)
-    {
-        $key = hex2bin(self::$encryptionKey); // Convertir clave de hexadecimal a binario
-        $decoded = base64_decode($string);
-        $iv_length = openssl_cipher_iv_length('aes-256-cbc');
-        $iv = substr($decoded, 0, $iv_length); // Extraer IV
-        $encrypted = substr($decoded, $iv_length); // Extraer texto cifrado
-        return openssl_decrypt($encrypted, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
-    }
 }
-?>
--- api/models/handler/recuperacion_handler.php
+++ api/models/handler/recuperacion_handler.php
@@ -37,7 +37,22 @@ class RecuperacionHandler
 
     public function envioCorreo()
     {
-        return Email::sendMail($this->idUsuario, $this->nivel, $this->hash, $this->correo, $this->nombre);
+        $titulo = '¡Bienvenid@ ' . $this->nombre . ', estamos aquí para ayudarte!';
+        $mailSubject = 'Recuperación de contraseña';	
+        $mailAltBody = 'Cambia tu contraseña con un solo click';
+        $link = 'http://localhost/sitio_gol_sv/views/admin/pages/change_passwords.html?c=' . urlencode($this->hash) . '&id=' . urlencode($this->idUsuario) . '&n=' . urlencode($this->nivel);
+        $message1 = '¡ingresa aquí!';
+        $message2 = 'Si no has solicitado este cambio, por favor ignora este mensaje. Este link solo estará habilitado durante los siguientes 15 minutos, después de este tiempo deberás solicitar un nuevo cambio de contraseña. Recuerda que tu seguridad es nuestra prioridad.';
+        $footer = 'Copyright &copy; ' . date("Y") . ' Fundación un gol para El Salvador. Todos los derechos reservados.';
+        // Cargar plantilla HTML
+        $template = file_get_contents('../../helpers/email/email.html');
+        // Reemplazar marcadores de posición con co1ntenido dinámico
+        $mailBody = str_replace(
+            ['{{subject}}', '{{title}}', '{{body}}', '{{link}}', '{{message1}}', '{{message2}}', '{{footer}}'],
+            [$mailSubject, $titulo, $mailAltBody, $link, $message1, $message2, $footer],
+            $template
+        );        
+        return Props::sendMail($this->correo, $mailSubject, $mailBody);
     }
     public function readHash()
     {
